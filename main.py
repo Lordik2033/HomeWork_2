509a3b651297626c980c10b9d6784644b96cbff7
Lordik <>
2022-01-14 14:06:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def find_prime():
+    num = 1
+    n = 69
+    while num <= n:
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                yield num
+        num += 1
+
+
+def find_odd_prime(seq):
+    for num in seq:
+        if (num % 2) != 0:
+            yield num
+
+
+a_pipeline = find_odd_prime(find_prime())
+
+for a_ele in a_pipeline:
+    print(a_ele)
+
